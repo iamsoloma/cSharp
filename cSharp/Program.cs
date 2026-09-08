1b3e4a5f5579d5112639d589a8c62029a7f6185b
Soloma <EgorSolomahin1@yandex.ru>
2026-09-08 22:42:03 +0500

    0 лаба

---
+++ cSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//using Newtonsoft.Json.Linq;
+
+namespace cSharp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Тебя зовут: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"{name}, здарова!");
+        //Console.ReadKey();
+
+        /*JObject o = JObject.FromObject(new
+        {
+            Name = name,
+            Addres = "Мухосраново"
+        });
+        Console.WriteLine(o.ToString());*/
+    }
+}
