67198e2582f93cb02cd094ce96872c951bc37000
Soloma <EgorSolomahin1@yandex.ru>
2026-09-16 08:48:42 +0500

    0 лаба. Надо было добавить проверку условий для ввода.

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -9,16 +9,46 @@ internal class Program
     {
 
         Console.Write("Тебя зовут: ");
-        string name = Console.ReadLine();
-
-        Console.WriteLine($"{name}, здарова!");
-        //Console.ReadKey();
-
-        /*JObject o = JObject.FromObject(new
+        string name = Console.ReadLine() ?? "";
+        if (name == "")
         {
-            Name = name,
-            Addres = "Мухосраново"
-        });
-        Console.WriteLine(o.ToString());*/
+            name = "Инкогнито";
+        }
+
+        Console.Write("А твой возраст?\n");
+        string ageLine = Console.ReadLine() ?? "";
+        int realAge;
+        if (!int.TryParse(ageLine, out realAge))
+        {
+            Console.WriteLine("Ты ввёл не число.");
+        }
+        else
+        {
+            if (realAge < 0)
+            {
+                Console.WriteLine("Ты явно ошибся.");
+            }
+            else
+            {
+                Console.WriteLine($"Здравствуй, {name}.");
+                if (realAge < 18)
+                {
+                    Console.WriteLine("Тебе запрещён алкоголь и энергетики!");
+                }
+                if (realAge < 13)
+                {
+                    Console.WriteLine("Тебе нельзя материться!");
+                }
+                if (realAge >= 18)
+                {
+                    Console.WriteLine("Ты совершенолетний.");
+                }
+                if (realAge > 35)
+                {
+                    Console.WriteLine("Тебе можно избираться в президенты.");
+                }
+            }
+        }
+
     }
 }
